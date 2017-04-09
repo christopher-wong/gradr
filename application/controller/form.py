7a788cd50af2d29b8e2d228939267636d16fb742
Christopher Wong <cwong@christopherwong.co>
2017-04-09 14:47:00 -0400

    add and delete from database.

--- application/controller/form.py
+++ application/controller/form.py
@@ -23,18 +23,7 @@ def db_insert_assignment_type(formInputs):
     a_type = "assignment_type"
     weight = "assignment_weight"
 
-    results = db_retrive_assignment_type()
-
-    s = set()
-
-    for document in results:
-        s.add(document['assignment_type'])
-        print(len(s))
-
-    if formInputs['assignment_type'] in s:
-        flash("This Assignment Type already exists")
-
-    elif a_type in formInputs and weight in formInputs:
+    if a_type in formInputs and weight in formInputs:
 
         try:
             db.assignmentType.insert({
@@ -62,35 +51,26 @@ def db_retrive_assignment_type():
 
 @form_bp.route('/')
 def index():
+    db_connect()
 
     return redirect('/home')
 
 
 @form_bp.route('/home')
 def home():
-    originalWeight = db_get_total_weight()
-    return render_template('index.html', originalWeight=originalWeight)
+    return render_template('index.html')
 
 
 @form_bp.route('/submit', methods=['POST'])
 def submit():
-    originalWeight = db_get_total_weight()
-
-
     # get inputs from form as dict
     if request.method == 'POST':
         formInputs = request.form.to_dict()
 
-    updatedWeight = originalWeight + int(formInputs['assignment_weight'])
+    db_insert_assignment_type(formInputs)
 
-    print(updatedWeight)
-
-    if updatedWeight <= 100:
-        db_insert_assignment_type(formInputs)
-    else:
-        flash("Sum of Weights Cannot Exceed 100")
-
-    return render_template('index.html', originalWeight=originalWeight)
+    return render_template('index.html',
+                           formInputs=formInputs)
 
 
 @form_bp.route('/view_database')
@@ -113,15 +93,3 @@ def db_remove_assignment_type():
     db.assignmentType.remove({'assignment_type': a_type})
 
     return redirect('/view_database')
-
-
-def db_get_total_weight():
-    results = db_retrive_assignment_type()
-
-    originalWeight = 0
-
-    for document in results:
-        # originalWeight = originalWeight + document.assignment_weight
-        originalWeight = originalWeight + int((document['assignment_weight']))
-
-    return originalWeight

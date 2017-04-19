d1bf76eaf4639c17292453cea87e2ebcc3351e1e
Christopher Wong <cwong@christopherwong.co>
2017-04-19 11:54:27 -0400

    final changes

--- runserver.py
+++ runserver.py
@@ -6,13 +6,11 @@ from application import app
 
 # Run the app
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 443))
+    port = int(os.environ.get("PORT", 5000))
     host = '0.0.0.0'
-    #host = '45.55.151.89'
     app.jinja_env.cache = {}
 
     app.run(host=host,
             port=port,
             debug=True,
             threaded=True)
-    #app.run(host='0.0.0.0')
